3912ed259ac3dbfef4f23e119936fddc8fb066c0
Crayon <210669314@qq.com>
2018-11-27 23:38:14 +0800

    update

--- src/common/js/song.js
+++ src/common/js/song.js
@@ -40,7 +40,7 @@ export function createSong(musicData) {
     album: musicData.albumname,
     duration: musicData.interval,
     image: `https://y.gtimg.cn/music/photo_new/T002R300x300M000${musicData.albummid}.jpg?max_age=2592000`,
-    url: `http://dl.stream.qqmusic.qq.com/C400${musicData.songmid}.m4a?guid=3989104853&vkey=ECE9BD3ADDA55CB5C26B9928CDF3B43F7C056F3E5286174641E4DB872501871F4E118113914548F8F2928C820743C8E789219A5731D53D36&uin=0&fromtag=38`
+    url: `http://dl.stream.qqmusic.qq.com/C400${musicData.songmid}.m4a?guid=3989104853&vkey=1E8C11991B86D75E6C9DAF8CA6B967C1FF2042504B9F04DCAC26A1616C462CB4DEA7F58221E313033A2EB6AC61FF01135DA11689BA9F520E&uin=0&fromtag=38`
     
   })
 }
